a260befcebb55ef8974c8109d3ba4e53bf2319f7
wolescode <dedotunwole@gmail.com>
2022-10-02 10:33:17 +0100

    Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.ayowolecode;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 100;
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " ");
+
+
+            }
+        }
+    }
+}
+
